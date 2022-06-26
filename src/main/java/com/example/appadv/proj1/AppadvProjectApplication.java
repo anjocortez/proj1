55d69fd07de07d8a5ffd09fb7e4712fe07b002f3
anjocortez <joseantonio.cortez@benilde.edu.ph>
2022-06-26 13:58:15 +0800

    Initial commit

---
+++ src/main/java/com/example/appadv/proj1/AppadvProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.appadv.proj1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppadvProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppadvProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/appadv/proj1/AppadvProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appadv.proj1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppadvProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
